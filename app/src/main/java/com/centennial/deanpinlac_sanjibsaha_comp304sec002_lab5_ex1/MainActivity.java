fa23ccd6ca8d877d7922f237ccbccad9d3fd6baf
Dean Pinlac <dean.pinlac@gmail.com>
2021-07-27 00:47:14 -0400

    Initial Commit

---
+++ app/src/main/java/com/centennial/deanpinlac_sanjibsaha_comp304sec002_lab5_ex1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.centennial.deanpinlac_sanjibsaha_comp304sec002_lab5_ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
